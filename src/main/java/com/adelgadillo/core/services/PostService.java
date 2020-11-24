ac330bc886e907c0ee552de015a6cb6e1bc6824e
Alejandro Delgadillo Garcia <4lejo.1017@gmail.com>
2020-11-23 19:54:38 -0500

    Commit: se agregan los scope

--- src/main/java/com/adelgadillo/core/services/PostService.java
+++ src/main/java/com/adelgadillo/core/services/PostService.java
@@ -7,6 +7,10 @@ import com.adelgadillo.core.model.Post;
 public interface PostService {
 
 	public List<Post> validation(List<Post> posts );
+
+	void addClass(Class clazz);
+
+	public Iterable<Post> validationId(List<Post> posts);
 	
 	
 }
--- src/main/java/com/adelgadillo/core/services/implementation/PostServiceDecoradoImp.java
+++ src/main/java/com/adelgadillo/core/services/implementation/PostServiceDecoradoImp.java
@@ -3,12 +3,16 @@ package com.adelgadillo.core.services.implementation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.adelgadillo.core.model.Post;
 import com.adelgadillo.core.services.PostService;
 
 @Service("serviceDecorado")
+@Scope("singleton")
+@Primary
 public class PostServiceDecoradoImp  implements PostService {
 
 	@Autowired
@@ -18,7 +22,31 @@ public class PostServiceDecoradoImp  implements PostService {
 	public List<Post> validation(List<Post> posts) {
 		
 		posts  = postServiceImpl.validation(posts);
-		return null;
+		for(Post post : posts) {
+			if(post.getDescription() == null) {
+				throw new NullPointerException("la descripcion esta nula");
+			}
+			if(post.getDate() == null) {
+				throw new NullPointerException("la fecha esta nula");
+			}
+		}
+			
+		return posts;
 	}
 
+	@Override
+	public void addClass(Class clazz) {
+		System.out.println(clazz.getName());
+	}
+
+	@Override
+	public List<Post> validationId(List<Post> posts) {
+
+		for(Post post : posts) {
+			if(post.getId() == 0) {
+				throw new NullPointerException("el id esta nulo");
+			}
+		}
+		return posts;
+	}
 }
